f5f3be6088f3a276dfd9de78f3fcfe679ea37e0b
Chaitanya-Raj <chaitanyarajdixit@gmail.com>
2019-03-25 22:27:34 +0530

    e

--- Java_Practicals/18/exc.java
+++ Java_Practicals/18/exc.java
@@ -3,16 +3,17 @@ import java.util.Scanner;
 public class exc {
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int a,b;
+        Scanner sc = new Scanner(System.in);
+        int a, b;
         System.out.println("\nEnter the dividend : ");
-        a=sc.nextInt();
+        a = sc.nextInt();
         System.out.println("\nEnter the divisor : ");
-        b=sc.nextInt();
+        b = sc.nextInt();
         try {
-            System.out.println(a+"/"+b+"="+(a/b));
+            System.out.println("\n" + a + "/" + b + "=" + (a / b));
         } catch (Exception e) {
             System.out.println(e);
         }
+        sc.close();
     }
 }
