76afd16c89783bc7187e67beb1ca871aa23d4f10
Chaitanya-Raj <chaitanyarajdixit@gmail.com>
2019-04-07 16:46:00 +0530

    prg8

--- Java_Practicals/8/prg8.java
+++ Java_Practicals/8/prg8.java
@@ -1,15 +1,12 @@
-import java.io.*;
-
 class prg8{
-    static public void main(String [] argv)throws IOException{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder s1=new StringBuilder("python is easy");
+    static public void main(String [] argv){
+        StringBuffer s1=new StringBuffer("python is easy");
         s1.setCharAt(1,'Y');
         System.out.println(s1);
-        System.out.println("the length of string s1 is"+s1.length());
+        System.out.println("the length of string s1 is "+s1.length());
         s1.append("java is hard");
         System.out.println(s1);
-        s1.insert(11,"python");
+        s1.insert(14," and ");
         System.out.println(s1);
         String s2="python";
         s2=s2.concat("hello world");
